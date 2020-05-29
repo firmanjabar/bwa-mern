0ad93b892f5599de1ccdc71f78bbcbce89cf8784
FirmanJabar <firmanabduljabar@gmail.com>
2020-05-29 18:41:08 +0800

    :sparkles: comp most fav & responsive

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import MostFav from "parts/MostFav";
 
+import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refFav = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props} />
-        <Hero />
+        <Hero refFav={this.refFav} />
+        <MostFav refFav={this.refFav} data={landingPage.mostFav} />
       </>
     );
   }
